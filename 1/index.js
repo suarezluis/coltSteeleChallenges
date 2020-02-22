cc83dcc947cb30d4e71b96f6d617f0e3ee9ff1a9
Luis Suarez <suarezluis@gmail.com>
2020-02-21 21:51:09 -0600

    Add comments to index.js

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,4 @@
+// Create a class
 class Step {
   constructor(element) {
     this.element = element;
@@ -18,23 +19,30 @@ class Step {
   };
 }
 
+// Create instances of Step for each step
 let step1 = new Step(document.getElementById("step1"));
 let step2 = new Step(document.getElementById("step2"));
 let step3 = new Step(document.getElementById("step3"));
 let step4 = new Step(document.getElementById("step4"));
 
+// Create array with indexes matching step numbers
 let steps = [0, step1, step2, step3, step4];
 
+// Create random 4 digit code for verification
 const randomCode = Math.floor(1000 + Math.random() * 8999);
 
-let code = (document.getElementById("code").innerHTML = randomCode);
+// Insert code to dom
+document.getElementById("code").innerHTML = randomCode;
 
+// Function that shows a step and close all others
 showStep = step => {
+  // Close all steps
   steps.forEach((step, index) => {
     index > 0 ? step.closeStep() : null;
   });
-
+  // Open selected step
   steps[step].openStep();
 };
 
+// Show first step by default
 step1.openStep();
